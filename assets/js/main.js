51baa8acb0c0b59220f1cf2c28bfb4b02449470d
mohammad-mekawy <mohammad.mekawy92@gmail.com>
2022-04-12 14:14:05 +0200

    appointment page

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,30 @@ $(document).ready(function(){
     }
     
 });
+//  disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
+//   country code
+  var input1 = document.querySelector("#validationTooltipUsernubmer");
+    window.intlTelInput(input1, {
+      preferredCountries: ['ae'],
+      separateDialCode: false,
+      nationalMode: false,
+    });
