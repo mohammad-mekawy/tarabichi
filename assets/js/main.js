e89c5bf3c7890a241625c1442bc451c27fdebdd6
mohammad-mekawy <mohammad.mekawy92@gmail.com>
2022-04-20 10:23:53 +0200

    tri2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,14 @@ $(document).ready(function(){
             $(this).addClass("T-large").removeClass("T-small").siblings().removeClass("T-large").addClass("T-small")
         }
     )
+    if ($(window).innerWidth() > 751) {
+        
     $(".nav-link").mouseenter(
         function(){
             let T_class= $(this).data("ball");
             $("#ball").removeAttr('class').addClass(T_class)
         }
     )
-    // var n=1;
-    // $(".doctors-images img").hover(
-    //     function(){
-    //         $(this).css({
-    //             "z-index":n++,
-    //             "filter":"unset"
-    //         }).siblings().css("filter","grayscale(1)")
-    //     }
-    // )
-    if ($(window).innerWidth() > 751) {
         $(".moaaz").hover(
             function(){
                 $(".sameih").css({
